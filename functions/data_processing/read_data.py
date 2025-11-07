74407fe7f3918aa2e74f970ced6ba6a04638c747
Kshitij Kar <kshitij797@gmail.com>
2025-11-07 14:28:04 +0100

    updated seismic signal processing

--- functions/data_processing/read_data.py
+++ functions/data_processing/read_data.py
@@ -13,45 +13,25 @@ except FileNotFoundError:
         data_params = json.load(file)
     with open("../config/event_id_map.json", "r") as file:
         time_config = json.load(file)
-import data
+
 import numpy as np
 import pandas as pd
+import obspy
+import obspy.signal
+import obspy.signal.filter
 from obspy import read, Stream, read_inventory
 from obspy.core import UTCDateTime # default is UTC+0 time zone
 
-def load_data(event_id_list:list, station:str, year:int=2019, trim:bool=True, abs:bool=True) -> np.array:
-    total_data = None
-    total_times = None
-    for event_id in event_id_list:
-        st = load_seismic_data(event_id = str(event_id), station= station, year=year, trim= trim)
-        data = st[0].data[1:]
-        times = st[0].times("matplotlib")[1:]
-        if total_data is None:
-            total_data = data
-            total_times = times
-        else:
-            total_data = np.concatenate([total_data, data])
-            total_times = np.concatenate([total_times, times])
-    if abs:
-        total_data = np.abs(total_data)
-    return total_data, total_times
+def moving_max(arr, N):
+    p = N // 2
+    arr_p = np.pad(arr, p, mode='edge')
+    stacked = [arr_p[i:i+arr.size] for i in range(N)]
+    return np.maximum.reduce(stacked)
 
-def load_data_test(julday_list:list, station:str, year:int=2019, abs:bool=True) -> np.array:
-    total_data = None
-    total_times = None
-    for julday in julday_list:
-        st = load_seismic_data_test(julday = julday, station= station, year=year)
-        data = st[0].data[1:]
-        times = st[0].times("matplotlib")[1:]
-        if total_data is None:
-            total_data = data
-            total_times = times
-        else:
-            total_data = np.concatenate([total_data, data])
-            total_times = np.concatenate([total_times, times])
-    if abs:
-        total_data = np.abs(total_data)
-    return total_data, total_times
+def gaussian_kernel(size, sigma=1.0):
+    x = np.linspace(- (size - 1) / 2, (size - 1) / 2, size)
+    kernel = np.exp(-0.5 * (x / sigma) ** 2)
+    return kernel / kernel.sum()
 
 def load_seismic_data(event_id:str|int, station:str, 
                       year:int=None, component:str='EHZ', network:str="9S", 
@@ -64,17 +44,17 @@ def load_seismic_data(event_id:str|int, station:str,
     # LOAD THE DATA AND SCALE
     if type(julday) is int:
         try:
-            st = read(f"{paths['BASE_DIR']}/{paths['DATA_DIR']}/{year}/{station}/{component}/9S.{station}.{component}.{year}.{str(julday).zfill(3)}.mseed")
+            st = read(f"{paths['BASE_DIR']}/{paths['DATA_DIR']}_{data_params['fmax']}/Illgraben/{year}/{station}/{component}/9S.{station}.{component}.{year}.{str(julday).zfill(3)}.mseed")
         except FileNotFoundError:
-            st = read(f"{paths['LOCAL_BASE_DIR']}/{paths['DATA_DIR']}/{year}/{station}/{component}/9S.{station}.{component}.{year}.{str(julday).zfill(3)}.mseed")
+            st = read(f"{paths['LOCAL_BASE_DIR']}/{paths['DATA_DIR']}_{data_params['fmax']}/Illgraben/{year}/{station}/{component}/9S.{station}.{component}.{year}.{str(julday).zfill(3)}.mseed")
         st[0].data = st[0].data * scaling
     elif type(julday) is list:
         st = Stream()
         for j in julday:
             try:
-                st += read(f"{paths['BASE_DIR']}/{paths['DATA_DIR']}/{year}/{station}/{component}/9S.{station}.{component}.{year}.{str(j).zfill(3)}.mseed")
+                st += read(f"{paths['BASE_DIR']}/{paths['DATA_DIR']}_{data_params['fmax']}/Illgraben/{year}/{station}/{component}/9S.{station}.{component}.{year}.{str(j).zfill(3)}.mseed")
             except:
-                st += read(f"{paths['LOCAL_BASE_DIR']}/{paths['DATA_DIR']}/{year}/{station}/{component}/9S.{station}.{component}.{year}.{str(j).zfill(3)}.mseed")
+                st += read(f"{paths['LOCAL_BASE_DIR']}/{paths['DATA_DIR']}_{data_params['fmax']}/Illgraben/{year}/{station}/{component}/9S.{station}.{component}.{year}.{str(j).zfill(3)}.mseed")
         st.merge(method=1, fill_value='latest', interpolation_samples=0)
         st[0].data = st[0].data * scaling
     else:
@@ -90,30 +70,86 @@ def load_seismic_data_test(julday:int|str|list, station:str,
                       year:int=None, component:str='EHZ', network:str="9S", 
                       ) -> Stream:
     scaling = 1e3
-    time_window = data_params['time_window']
-    # event_id = str(event_id)
-    # event_info = time_config[event_id]
-    # julday = event_info['julday']
     # LOAD THE DATA AND SCALE
     if type(julday) is int or type(julday) is str:
         try:
-            st = read(f"{paths['BASE_DIR']}/{paths['DATA_DIR']}/{year}/{station}/{component}/{network}.{station}.{component}.{year}.{str(julday).zfill(3)}.mseed")
+            st = read(f"{paths['BASE_DIR']}/{paths['DATA_DIR']}_{data_params['fmax']}/Illgraben/{year}/{station}/{component}/{network}.{station}.{component}.{year}.{str(julday).zfill(3)}.mseed")
         except FileNotFoundError:
-            st = read(f"{paths['LOCAL_BASE_DIR']}/{paths['DATA_DIR']}/{year}/{station}/{component}/{network}.{station}.{component}.{year}.{str(julday).zfill(3)}.mseed")
+            st = read(f"{paths['LOCAL_BASE_DIR']}/{paths['DATA_DIR']}_{data_params['fmax']}/Illgraben/{year}/{station}/{component}/{network}.{station}.{component}.{year}.{str(julday).zfill(3)}.mseed")
         st[0].data = st[0].data * scaling
     elif type(julday) is list:
         st = Stream()
         for j in julday:
             try:
-                st += read(f"{paths['BASE_DIR']}/{paths['DATA_DIR']}/{year}/{station}/{component}/{network}.{station}.{component}.{year}.{str(j).zfill(3)}.mseed")
+                st += read(f"{paths['BASE_DIR']}/{paths['DATA_DIR']}_{data_params['fmax']}/Illgraben/{year}/{station}/{component}/{network}.{station}.{component}.{year}.{str(j).zfill(3)}.mseed")
             except:
-                st += read(f"{paths['LOCAL_BASE_DIR']}/{paths['DATA_DIR']}/{year}/{station}/{component}/{network}.{station}.{component}.{year}.{str(j).zfill(3)}.mseed")
+                st += read(f"{paths['LOCAL_BASE_DIR']}/{paths['DATA_DIR']}_{data_params['fmax']}/Illgraben/{year}/{station}/{component}/{network}.{station}.{component}.{year}.{str(j).zfill(3)}.mseed")
         st.merge(method=1, fill_value='latest', interpolation_samples=0)
         st[0].data = st[0].data * scaling
     else:
         print(f"Wrong julday type : {type(julday)}")
         raise TypeError
     return st
+
+def load_data(event_id_list:list, station:str, year:int=2019, trim:bool=True, abs:bool=True, env:bool=True) -> np.array:
+    total_data = None
+    total_times = None
+    for event_id in event_id_list:
+        st = load_seismic_data(event_id = str(event_id), station= station, year=year, trim= trim)
+        if env:
+            data_envelope = obspy.signal.filter.envelope(st[0].data)
+            # num_secs = 0.5
+            # window = int(2*100*num_secs)
+            # kernel = gaussian_kernel(window, sigma=3)
+            # data = np.convolve(data_envelope, kernel, mode='same')
+            # num_seconds = 0.03125
+            # N = int(2 * 100 * num_seconds)
+            # data = moving_max(data_envelope, N)[1:]
+            data = data_envelope
+            data = data[1:]
+        else:
+            data = st[0].data[1:]
+        times = st[0].times("matplotlib")[1:]
+        if total_data is None:
+            total_data = data
+            total_times = times
+        else:
+            total_data = np.concatenate([total_data, data])
+            total_times = np.concatenate([total_times, times])
+    if abs:
+        total_data = np.abs(total_data)
+    return total_data, total_times
+
+def load_data_test(julday_list:list, station:str, year:int=2019, abs:bool=True, env:bool=True) -> np.array:
+    total_data = None
+    total_times = None
+    for julday in julday_list:
+        st = load_seismic_data_test(julday = julday, station= station, year=year)
+        if env:
+            data_envelope = obspy.signal.filter.envelope(st[0].data)
+            # num_secs = 0.5
+            # window = int(2*100*num_secs)
+            # kernel = gaussian_kernel(window, sigma=3)
+            # data = np.convolve(data_envelope, kernel, mode='same')
+            # num_seconds = 0.03125
+            # N = int(2 * 100 * num_seconds)
+            # data = moving_max(data_envelope, N)[1:]
+            data = data_envelope
+            data = data[1:]
+        else:
+            data = st[0].data[1:]
+        times = st[0].times("matplotlib")[1:]
+        if total_data is None:
+            total_data = data
+            total_times = times
+        else:
+            total_data = np.concatenate([total_data, data])
+            total_times = np.concatenate([total_times, times])
+    if abs:
+        total_data = np.abs(total_data)
+    return total_data, total_times
+
+
 
 def load_label(event_id_list: list, station: str, interval_seconds: int, time_shift_minutes, trim:bool = True, smoothing: int | None = 30, divide_by: int | None = 350) -> pd.DataFrame:
     time_window = data_params['time_window']
