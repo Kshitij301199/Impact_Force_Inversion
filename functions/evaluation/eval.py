8acc9fdcf076aad9abc7e7514b47012b6b20b8b6
Kshitij Kar <kshitij797@gmail.com>
2025-11-12 13:51:28 +0100

    updated hist-wmse in eval

--- functions/evaluation/eval.py
+++ functions/evaluation/eval.py
@@ -24,12 +24,12 @@ plt.rcParams.update({
 
 from functions.data_processing.read_data import load_label
 
-def weighted_mse(weights, h1, h2):
-    assert len(weights) == len(h1) == len(h2), "Length mismatch in weighted mse calculation"
-    n = len(h1)
-    wse = np.dot(weights, (h1 - h2) ** 2)
-    wmse = wse / n
-    return wmse
+# def weighted_mse(weights, h1, h2):
+#     assert len(weights) == len(h1) == len(h2), "Length mismatch in weighted mse calculation"
+#     n = len(h1)
+#     wse = np.dot(weights, (h1 - h2) ** 2)
+#     wmse = wse / n
+#     return wmse
 
 def evaluate_model(model_type:str, test_id:str, val_id:str, interval_seconds:int, y_true, y_pred, smoothing:int, out_dir:str, time_to_train:str):
     print(f"{'Evaluating Model':-^50}")
@@ -43,38 +43,35 @@ def evaluate_model(model_type:str, test_id:str, val_id:str, interval_seconds:int
     dist_dir = f"{out_dir}/dist_plots/test/{interval_seconds}/{test_julday}"
     os.makedirs(dist_dir, exist_ok=True)
     os.makedirs(output_dir, exist_ok=True)
-    # filename = f"{output_dir}/evaluation_output.txt"
+    filename = f"{output_dir}/evaluation_output_wo_noise.txt"
     filename2 = f"{output_dir}/evaluation_output_constrained.txt"
     try:
-        # with open(filename, "x") as file:  # "x" mode creates a file if it does not exist
-        #     file.write("Model,Config,Time_To_Train,Test,Val,Interval,MSE,RMSE,MAE,R2,Corr,PearsonR\n")
-        #     print(f"File '{filename}' created with columns names")
+        with open(filename, "x") as file:  # "x" mode creates a file if it does not exist
+            file.write("Model,Config,Time_To_Train,Test,Val,Interval,MSE,RMSE,MAE,R2,Corr,PearsonR,Hist_WMSE\n")
+            print(f"File '{filename}' created with columns names")
         with open(filename2, "x") as file:  # "x" mode creates a file if it does not exist
             file.write("Model,Config,Time_To_Train,Test,Val,Interval,MSE,RMSE,MAE,R2,Corr,PearsonR,Hist_WMSE\n")
             print(f"File '{filename2}' created with columns names")
     except FileExistsError:
         pass
 
-    zero_label = load_label([test_id], "ILL11", interval_seconds, 0, trim=False, smoothing=smoothing)
+    zero_label = load_label([test_id], "ILL11", interval_seconds, 0, trim=True, smoothing=smoothing)
     zero_label['Timestamp'] = zero_label['Timestamp'].apply(UTCDateTime)
-    zero_label = zero_label.iloc[:len(y_true)]
+    # print(f"During evaluation, line 60, zero_label len : {len(zero_label)} and y_pred len : {len(y_pred)}")
+    # zero_label = zero_label.iloc[:len(y_true)]
     zero_label['True_Value'] = y_true
     zero_label['Pred_Value'] = y_pred
     
-    zero_label['Timestamp'] = zero_label['Timestamp'].between(UTCDateTime(test_info['start_time']), UTCDateTime(test_info['end_time']))
-
+    # zero_label['Timestamp'] = zero_label['Timestamp'].between(UTCDateTime(test_info['start_time']), UTCDateTime(test_info['end_time']))
     fig, ax = plt.subplots()
     bins = np.arange(3, 45, 2)
-    heights1, width, _ = ax.hist(zero_label['True_Value'].to_numpy(), bins=bins, color='red', alpha=0.8, label="Impact Force [kN]")
-    heights2, _, _ = ax.hist(zero_label['Pred_Value'].to_numpy(), bins=bins, color='blue', alpha=0.6, label="Model Prediction")
-    centers = width[:-1] + (width[1:] - width[:-1]) / 2
-    weights = centers / 45
-    ax.set_xlabel("Normal Force [kN]")
-    ax.set_ylabel("Count")
-    ax.set_title(f"{model_type.split(",")[0]} {interval_seconds} test {test_julday} val {val_julday}")
-    ax.legend(loc='best')
-    fig.savefig(f"{dist_dir}/{model_type.split(",")[0]}_{val_julday}.png", dpi=300)
+    heights1, width, _ = ax.hist(zero_label['True_Value'].to_numpy(), bins=bins, color='red', alpha=0.8, label="Impact Force [kN]", density=True)
+    heights2, _, _ = ax.hist(zero_label['Pred_value'].to_numpy(), bins=bins, color='blue', alpha=0.6, label="Model Prediction", density=True)
+    n = len(bins)
+    fig.tight_layout()
     plt.close(fig=fig)
+    centers = width[:-1] + (width[1:] - width[:-1]) / 2
+    weights = centers
 
     r1, _ = pearsonr(zero_label['True_Value'].to_numpy(), zero_label['Pred_Value'].to_numpy())
     corr1 = np.correlate(zero_label['True_Value'].to_numpy() - np.mean(zero_label['True_Value'].to_numpy()), zero_label['Pred_Value'].to_numpy() - np.mean(zero_label['Pred_Value'].to_numpy()), mode='full')
@@ -89,7 +86,36 @@ def evaluate_model(model_type:str, test_id:str, val_id:str, interval_seconds:int
                 f"{r2_score(zero_label['True_Value'].to_numpy(), zero_label['Pred_Value'].to_numpy()):.4f},"
                 f"{lag1:.4f},"
                 f"{r1:.4f},"
-                f"{weighted_mse(weights, heights1, heights2):.4f}\n"
+                f"{np.dot(weights, np.sqrt((heights2 - heights1) ** 2)) / n:.4f}\n"
+                    )
+        f.write(string)
+
+    # To remove the effect of noise, apply a threshold
+    zero_label = zero_label[zero_label['True_Value'] >= 3]
+    r1, _ = pearsonr(zero_label['True_Value'].to_numpy(), zero_label['Pred_Value'].to_numpy())
+    corr1 = np.correlate(zero_label['True_Value'].to_numpy() - np.mean(zero_label['True_Value'].to_numpy()), zero_label['Pred_Value'].to_numpy() - np.mean(zero_label['Pred_Value'].to_numpy()), mode='full')
+    lag1 = np.argmax(corr1) - (len(zero_label['True_Value'].to_numpy()) - 1)
+
+    fig, ax = plt.subplots()
+    bins = np.arange(3, 45, 2)
+    heights1, width, _ = ax.hist(zero_label['True_Value'].to_numpy(), bins=bins, color='red', alpha=0.8, label="Impact Force [kN]", density=True)
+    heights2, _, _ = ax.hist(zero_label['Pred_value'].to_numpy(), bins=bins, color='blue', alpha=0.6, label="Model Prediction", density=True)
+    n = len(bins)
+    fig.tight_layout()
+    plt.close(fig=fig)
+    centers = width[:-1] + (width[1:] - width[:-1]) / 2
+    weights = centers
+
+    with open(filename, "a") as f:
+        string = (
+                f"{model_type},{time_to_train},{test_julday},{val_julday},{interval_seconds},"
+                f"{mean_squared_error(zero_label['True_Value'].to_numpy(), zero_label['Pred_Value'].to_numpy()):.4f},"
+                f"{np.sqrt(mean_squared_error(zero_label['True_Value'].to_numpy(), zero_label['Pred_Value'].to_numpy())):.4f},"
+                f"{mean_absolute_error(zero_label['True_Value'].to_numpy(), zero_label['Pred_Value'].to_numpy()):.4f},"
+                f"{r2_score(zero_label['True_Value'].to_numpy(), zero_label['Pred_Value'].to_numpy()):.4f},"
+                f"{lag1:.4f},"
+                f"{r1:.4f},"
+                f"{np.dot(weights, np.sqrt((heights2 - heights1) ** 2)) / n:.4f}\n"
                     )
         f.write(string)
     return None
