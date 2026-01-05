838e71f0607301f0dc21c85dacd216d7c1b4b919
Kshitij Kar <kshitij797@gmail.com>
2026-01-05 12:21:19 +0100

    updated sbatch files

--- sbatch/cb_traintest/train_lstm_single.sh
+++ sbatch/cb_traintest/train_lstm_single.sh
@@ -18,9 +18,9 @@ conda activate xlstm_env
 
 intervals=(5)
 event_ids=(1 3 4 5 6 7 8 9)
-divide_bys=(45)
-hyp_options=('smaller' 'larger')
-smoothings=(60)
+divide_bys=(0 20 45)
+hyp_options=('default')
+smoothings=(0 30 60)
 
 num_intervals=${#intervals[@]}
 num_hyp_options=${#hyp_options[@]}
