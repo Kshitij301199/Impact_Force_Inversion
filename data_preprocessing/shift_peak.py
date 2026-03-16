ead4898da85767d864da5cd26ab639818e43646d
Kshitij Kar <kshitij797@gmail.com>
2026-03-16 12:15:49 +0100

    fixed bugs in data alignment

--- data_preprocessing/shift_peak.py
+++ data_preprocessing/shift_peak.py
@@ -30,26 +30,19 @@ def main(time_shift_minutes, from_velocity:bool=False, avg_peak_shift:bool=False
     station = "ILL11"
     input_dir = f"{paths['LOCAL_BASE_DIR']}/{paths['UTC0_LABEL_DIR']}"
     velocity_dir = f"{paths['LOCAL_BASE_DIR']}/label"
-    event_data = pd.read_csv("2019-event-times.csv", index_col=0)
-    min_Fv, min_Fv_std = get_mean_and_std()
+    # event_data = pd.read_csv("2019-event-times.csv", index_col=0)
+    min_Fv, min_Fv_std = get_mean_and_std() # defined in utils file
     # min_Fv, min_Fv_std = 0.5, 0.01
-    sigma = 5
+    sigma = 3
     print(f"Minimum Impact Force : {min_Fv:.2f}kN, \nMinimum Standard Deviation : {min_Fv_std:.2f}kN, Used Standard Deviation : {sigma * min_Fv_std:.2f}kN")
     
-    if not from_velocity:
+    if not from_velocity: # CONSTANT TIME SHIFT BETWEEN SEISMIC AND IF PEAK
         output_dir = f"{paths['LOCAL_BASE_DIR']}/label/data_processed_{time_shift_minutes}/{station}"
         os.makedirs(output_dir, exist_ok=True)
         print("Running Unique Case 1")
         start_date = "2019-06-10"
-        end_date = "2019-06-11"
-        df_start_times = event_data[event_data[f'{station}Start'].apply(UTCDateTime).between(UTCDateTime(start_date),
-                                                                                            UTCDateTime(end_date))][f'{station}Start']
-        df_start_times = df_start_times.apply(UTCDateTime)
         data1 = pd.read_csv(f"{input_dir}/20190610_Fv_1.csv", index_col=0)
         data2 = pd.read_csv(f"{input_dir}/20190610_Fv_2.csv", index_col=0)
-        data_start_time1, data_start_time2 = UTCDateTime(data1.iloc[0,-1]), UTCDateTime(data2.iloc[0,-1])
-        # print(f"{data_start_time1.datetime} \n{data_start_time2.datetime}")
-        time_diff1, time_diff2 = data_start_time1 - df_start_times.iloc[0], data_start_time2 - df_start_times.iloc[1]
 
         data1['Time'] = data1.iloc[:,-1].apply(UTCDateTime) - (5.2 * 60) + (time_shift_minutes * 60)
         data2['Time'] = data2.iloc[:,-1].apply(UTCDateTime) - (2.1 * 60) + (time_shift_minutes * 60)
@@ -72,15 +65,11 @@ def main(time_shift_minutes, from_velocity:bool=False, avg_peak_shift:bool=False
         print("Running Unique Case 2")
         DataStart = "2019-07-01"
         DataEnd = "2019-07-04"
-        df_start_times = event_data[event_data[f'{station}Start'].apply(UTCDateTime).between(UTCDateTime(DataStart), UTCDateTime(DataEnd))][f'{station}Start']
-        df_start_times = df_start_times.apply(UTCDateTime)
         data1 = pd.read_csv(f"{input_dir}/20190702_Fv.csv", index_col=0)
         data2 = pd.read_csv(f"{input_dir}/20190703_Fv.csv", index_col=0)
         data_start_time1, data_start_time2 = UTCDateTime(data1.iloc[0,-1]), UTCDateTime(data2.iloc[0,-1])
-        # print(f"{data_start_time1.datetime} \n{data_start_time2.datetime}")
-        time_diff1, time_diff2 = data_start_time1 - df_start_times.iloc[0], data_start_time2 - df_start_times.iloc[1]
-        data1['Time'] = data1.iloc[:,-1].apply(UTCDateTime) - (61.2 * 60) + (time_shift_minutes * 60)
-        data2['Time'] = data2.iloc[:,-1].apply(UTCDateTime) - (68.1 * 60) + (time_shift_minutes * 60)
+        data1['Time'] = data1.iloc[:,-1].apply(UTCDateTime) - (1.2 * 60) + (time_shift_minutes * 60)
+        data2['Time'] = data2.iloc[:,-1].apply(UTCDateTime) - (8.1 * 60) + (time_shift_minutes * 60)
 
         data = pd.concat([data1, data2])
         complete_data = pd.DataFrame(columns= ['Time'])
@@ -99,22 +88,16 @@ def main(time_shift_minutes, from_velocity:bool=False, avg_peak_shift:bool=False
 
         # General Cases
         print("Running General Case")
-        # peak_shift_df = pd.read_csv("peak_times.csv", index_col=False)
         date_starts = ["2019-06-21","2019-07-15","2019-07-26","2019-08-11","2019-08-20"]
         date_ends = ["2019-06-22","2019-07-16","2019-07-27","2019-08-12","2019-08-21"]
         shift_values = [time_shift_minutes] * 5
-        # peak_diff_values = [59, 69, 61, 57, 62] # OLD
-        peak_diff_values = [61.2, 69.5, 61.3, 57.5, 62.4]
+        # peak_diff_values = [61.2, 69.5, 61.3, 57.5, 62.4]
+        peak_diff_values = [1.2, 9.5, 1.3, -2.5, 2.4]
 
         for date_start, date_end, shift, peak_diff in tqdm(zip(date_starts, date_ends, shift_values, peak_diff_values), total=5):
-            DataStart = date_start
-            DataEnd = date_end
-            df_start_times = event_data[event_data[f'{station}Start'].apply(UTCDateTime).between(UTCDateTime(DataStart), UTCDateTime(DataEnd))][f'{station}Start']
-            df_start_times = df_start_times.apply(UTCDateTime)
+            DataStart, DataEnd = date_start, date_end
             data = pd.read_csv(f"{input_dir}/{date_start.replace('-','')}_Fv.csv", index_col=0)
             data_start_time1 = UTCDateTime(data.iloc[0,-1])
-            # print(data_start_time1.datetime)
-            time_diff1 = data_start_time1 - df_start_times.iloc[0] 
             data['Time'] = data.iloc[:,-1].apply(UTCDateTime) - (peak_diff * 60) + (shift * 60)
             complete_data = pd.DataFrame(columns= ['Time'])
             start_time = UTCDateTime(date_start)
@@ -127,30 +110,24 @@ def main(time_shift_minutes, from_velocity:bool=False, avg_peak_shift:bool=False
             merged_data['Fv [kN]'] = merged_data['Fv [kN]'].apply(fill_missing_from_Gaussian, **{"mean": min_Fv, "std": sigma*min_Fv_std})
             merged_data = merged_data.fillna(value={"Fv std": min_Fv_std, "Fv min": 0, "Fv max": 0})
             merged_data.to_csv(f"{output_dir}/{date_start}.csv", index=False)
-    elif from_velocity and not avg_peak_shift:
+
+    elif from_velocity and not avg_peak_shift: # USE VELOCITY BUT HAVE DIFFERENT SHIFTS FOR DIFFERENT EVENTS
         output_dir = f"{paths['LOCAL_BASE_DIR']}/label/data_processed_dynamic/{station}"
         os.makedirs(output_dir, exist_ok=True)
         distance = 450 # in meters between ILL11 and Force Plate
-        vel_df = pd.read_csv(f"{velocity_dir}/DF_characteristics.csv", index_col=False)
-        vel_df['Event_Date'] = vel_df['Event_Date'].apply(lambda x: x.split('T')[0])
+        vel_df = pd.read_csv(f"{velocity_dir}/DF_Characteristics_2019-2023.csv", index_col=0)
         print("Running Unique Case 1")
         start_date = "2019-06-10"
         end_date = "2019-06-11"
         temp = vel_df[vel_df['Event_Date'] == start_date]
-        df_start_times = event_data[event_data[f'{station}Start'].apply(UTCDateTime).between(UTCDateTime(start_date),
-                                                                                            UTCDateTime(end_date))][f'{station}Start']
-        df_start_times = df_start_times.apply(UTCDateTime)
         data1 = pd.read_csv(f"{input_dir}/20190610_Fv_1.csv", index_col=0)
         data2 = pd.read_csv(f"{input_dir}/20190610_Fv_2.csv", index_col=0)
-        data_start_time1, data_start_time2 = UTCDateTime(data1.iloc[0,-1]), UTCDateTime(data2.iloc[0,-1])
-        # print(f"{data_start_time1.datetime} \n{data_start_time2.datetime}")
-        time_diff1, time_diff2 = data_start_time1 - df_start_times.iloc[0], data_start_time2 - df_start_times.iloc[1]
 
-        print(f"date: {start_date}, vel: {temp.iloc[0,2]}, time_shift: {distance / temp.iloc[0,2] : .2f}")
-        print(f"date: {start_date}, vel: {temp.iloc[1,2]}, time_shift: {distance / temp.iloc[1,2] : .2f}")
+        print(f"date: {start_date}, vel: {temp.iloc[0,4]}, time_shift: {distance / temp.iloc[0,4] : .2f}")
+        print(f"date: {start_date}, vel: {temp.iloc[1,4]}, time_shift: {distance / temp.iloc[1,4] : .2f}")
 
-        data1['Time'] = data1.iloc[:,-1].apply(UTCDateTime) - (5.2 * 60) + int(distance / temp.iloc[0,2]) # - peak_to_peak difference + time shift by velocity
-        data2['Time'] = data2.iloc[:,-1].apply(UTCDateTime) - (2.1 * 60) + int(distance / temp.iloc[1,2]) # - peak_to_peak difference + time shift by velocity
+        data1['Time'] = data1.iloc[:,-1].apply(UTCDateTime) - (5.2 * 60) + int(distance / temp.iloc[0,4]) # - peak_to_peak difference + time shift by velocity
+        data2['Time'] = data2.iloc[:,-1].apply(UTCDateTime) - (2.1 * 60) + int(distance / temp.iloc[1,4]) # - peak_to_peak difference + time shift by velocity
         data = pd.concat([data1, data2])
         complete_data = pd.DataFrame(columns= ['Time'])
         start_time = UTCDateTime("2019-06-10T00:00:00")
@@ -170,17 +147,12 @@ def main(time_shift_minutes, from_velocity:bool=False, avg_peak_shift:bool=False
         DataStart = "2019-07-01"
         DataEnd = "2019-07-04"
         temp = vel_df[(vel_df['Event_Date'] == "2019-07-02") | (vel_df['Event_Date'] == "2019-07-03")]
-        df_start_times = event_data[event_data[f'{station}Start'].apply(UTCDateTime).between(UTCDateTime(DataStart), UTCDateTime(DataEnd))][f'{station}Start']
-        df_start_times = df_start_times.apply(UTCDateTime)
         data1 = pd.read_csv(f"{input_dir}/20190702_Fv.csv", index_col=0)
         data2 = pd.read_csv(f"{input_dir}/20190703_Fv.csv", index_col=0)
-        data_start_time1, data_start_time2 = UTCDateTime(data1.iloc[0,-1]), UTCDateTime(data2.iloc[0,-1])
-        print(f"date: {DataStart}, vel: {temp.iloc[0,2]}, time_shift: {distance / temp.iloc[0,2] : .2f}")
-        print(f"date: {DataStart}, vel: {temp.iloc[1,2]}, time_shift: {distance / temp.iloc[1,2] : .2f}")
-        # print(f"{data_start_time1.datetime} \n{data_start_time2.datetime}")
-        time_diff1, time_diff2 = data_start_time1 - df_start_times.iloc[0], data_start_time2 - df_start_times.iloc[1]
-        data1['Time'] = data1.iloc[:,-1].apply(UTCDateTime) - (61.95 * 60) + int(distance / temp.iloc[0,2])
-        data2['Time'] = data2.iloc[:,-1].apply(UTCDateTime) - (68.1 * 60) + int(distance / temp.iloc[1,2])
+        print(f"date: {DataStart}, vel: {temp.iloc[0,4]}, time_shift: {distance / temp.iloc[0,4] : .2f}")
+        print(f"date: {DataStart}, vel: {temp.iloc[1,4]}, time_shift: {distance / temp.iloc[1,4] : .2f}")
+        data1['Time'] = data1.iloc[:,-1].apply(UTCDateTime) - (1.95 * 60) + int(distance / temp.iloc[0,4])
+        data2['Time'] = data2.iloc[:,-1].apply(UTCDateTime) - (8.1 * 60) + int(distance / temp.iloc[1,4])
         data = pd.concat([data1, data2])
         complete_data = pd.DataFrame(columns= ['Time'])
         start_time = UTCDateTime("2019-07-01T00:00:00")
@@ -198,23 +170,17 @@ def main(time_shift_minutes, from_velocity:bool=False, avg_peak_shift:bool=False
 
         # General Cases
         print("Running General Case")
-        # peak_shift_df = pd.read_csv("peak_times.csv", index_col=False)
         date_starts = ["2019-06-21","2019-07-15","2019-07-26","2019-08-11","2019-08-20"]
         date_ends = ["2019-06-22","2019-07-16","2019-07-27","2019-08-12","2019-08-21"]
-        # peak_diff_values = [60, 68, 62, 57, 62]
-        peak_diff_values = [61.2, 69.5, 61.3, 57.5, 62.5]
+        # peak_diff_values = [61.2, 69.5, 61.3, 57.5, 62.4]
+        peak_diff_values = [1.2, 9.5, 1.3, -2.5, 2.4]
+
         for date_start, date_end, peak_diff in tqdm(zip(date_starts, date_ends, peak_diff_values), total=5):
             temp = vel_df[vel_df['Event_Date'] == date_start]
-            DataStart = date_start
-            DataEnd = date_end
-            df_start_times = event_data[event_data[f'{station}Start'].apply(UTCDateTime).between(UTCDateTime(DataStart), UTCDateTime(DataEnd))][f'{station}Start']
-            df_start_times = df_start_times.apply(UTCDateTime)
+            DataStart, DataEnd = date_start, date_end
             data = pd.read_csv(f"{input_dir}/{date_start.replace('-','')}_Fv.csv", index_col=0)
-            data_start_time1 = UTCDateTime(data.iloc[0,-1])
-            # print(data_start_time1.datetime)
-            time_diff1 = data_start_time1 - df_start_times.iloc[0] 
-            print(f"date: {date_start}, vel: {temp.iloc[0,2]}, time_shift: {distance / temp.iloc[0,2] : .2f}")
-            data['Time'] = data.iloc[:,-1].apply(UTCDateTime) - (peak_diff * 60) + int(distance / temp.iloc[0,2])
+            print(f"date: {date_start}, vel: {temp.iloc[0,2]}, time_shift: {distance / temp.iloc[0,4] : .2f}")
+            data['Time'] = data.iloc[:,-1].apply(UTCDateTime) - (peak_diff * 60) + int(distance / temp.iloc[0,4])
             complete_data = pd.DataFrame(columns= ['Time'])
             start_time = UTCDateTime(date_start)
             time_list1 = []
@@ -226,26 +192,20 @@ def main(time_shift_minutes, from_velocity:bool=False, avg_peak_shift:bool=False
             merged_data['Fv [kN]'] = merged_data['Fv [kN]'].apply(fill_missing_from_Gaussian, **{"mean": min_Fv, "std": sigma*min_Fv_std})
             merged_data = merged_data.fillna(value={"Fv std": min_Fv_std, "Fv min": 0, "Fv max": 0})
             merged_data.to_csv(f"{output_dir}/{date_start}.csv", index=False)
-    elif from_velocity and avg_peak_shift:
+
+    elif from_velocity and avg_peak_shift: # USE VELOCITY TO DETERMINE THE AVERAGE SHIFT TO ALIGN SEIS AND IF
         output_dir = f"{paths['LOCAL_BASE_DIR']}/label/data_processed_average/{station}"
         os.makedirs(output_dir, exist_ok=True)
         distance = 450 # in meters between ILL11 and Force Plate
-        vel_df = pd.read_csv(f"{velocity_dir}/DF_characteristics.csv", index_col=False)
-        vel_df['Event_Date'] = vel_df['Event_Date'].apply(lambda x: x.split('T')[0])
+        vel_df = pd.read_csv(f"{velocity_dir}/DF_Characteristics_2019-2023.csv", index_col=0)
+        vel_df = vel_df[vel_df["Event_Date"] != "2023-07-12"]
         avg_velocity = np.round(vel_df['Velocity'].mean())
         print(f"Average Velocity : {avg_velocity:.2f} m/s, Time Shift : {distance / avg_velocity:.2f} seconds")
         print("Running Unique Case 1")
         start_date = "2019-06-10"
-        end_date = "2019-06-11"
         temp = vel_df[vel_df['Event_Date'] == start_date]
-        df_start_times = event_data[event_data[f'{station}Start'].apply(UTCDateTime).between(UTCDateTime(start_date),
-                                                                                            UTCDateTime(end_date))][f'{station}Start']
-        df_start_times = df_start_times.apply(UTCDateTime)
         data1 = pd.read_csv(f"{input_dir}/20190610_Fv_1.csv", index_col=0)
         data2 = pd.read_csv(f"{input_dir}/20190610_Fv_2.csv", index_col=0)
-        data_start_time1, data_start_time2 = UTCDateTime(data1.iloc[0,-1]), UTCDateTime(data2.iloc[0,-1])
-        # print(f"{data_start_time1.datetime} \n{data_start_time2.datetime}")
-        time_diff1, time_diff2 = data_start_time1 - df_start_times.iloc[0], data_start_time2 - df_start_times.iloc[1]
 
         print(f"date: {start_date}, vel: {avg_velocity}, time_shift: {distance / avg_velocity : .2f}")
         print(f"date: {start_date}, vel: {avg_velocity}, time_shift: {distance / avg_velocity : .2f}")
@@ -269,19 +229,13 @@ def main(time_shift_minutes, from_velocity:bool=False, avg_peak_shift:bool=False
         # Unique Cases 2
         print("Running Unique Case 2")
         DataStart = "2019-07-01"
-        DataEnd = "2019-07-04"
         temp = vel_df[(vel_df['Event_Date'] == "2019-07-02") | (vel_df['Event_Date'] == "2019-07-03")]
-        df_start_times = event_data[event_data[f'{station}Start'].apply(UTCDateTime).between(UTCDateTime(DataStart), UTCDateTime(DataEnd))][f'{station}Start']
-        df_start_times = df_start_times.apply(UTCDateTime)
         data1 = pd.read_csv(f"{input_dir}/20190702_Fv.csv", index_col=0)
         data2 = pd.read_csv(f"{input_dir}/20190703_Fv.csv", index_col=0)
-        data_start_time1, data_start_time2 = UTCDateTime(data1.iloc[0,-1]), UTCDateTime(data2.iloc[0,-1])
         print(f"date: {DataStart}, vel: {avg_velocity}, time_shift: {distance / avg_velocity : .2f}")
         print(f"date: {DataStart}, vel: {avg_velocity}, time_shift: {distance / avg_velocity : .2f}")
-        # print(f"{data_start_time1.datetime} \n{data_start_time2.datetime}")
-        time_diff1, time_diff2 = data_start_time1 - df_start_times.iloc[0], data_start_time2 - df_start_times.iloc[1]
-        data1['Time'] = data1.iloc[:,-1].apply(UTCDateTime) - (61.95 * 60) + int(distance / avg_velocity)
-        data2['Time'] = data2.iloc[:,-1].apply(UTCDateTime) - (68.1 * 60) + int(distance / avg_velocity)
+        data1['Time'] = data1.iloc[:,-1].apply(UTCDateTime) - (1.95 * 60) + int(distance / avg_velocity)
+        data2['Time'] = data2.iloc[:,-1].apply(UTCDateTime) - (8.1 * 60) + int(distance / avg_velocity)
 
         data = pd.concat([data1, data2])
         complete_data = pd.DataFrame(columns= ['Time'])
@@ -300,22 +254,15 @@ def main(time_shift_minutes, from_velocity:bool=False, avg_peak_shift:bool=False
 
         # General Cases
         print("Running General Case")
-        # peak_shift_df = pd.read_csv("peak_times.csv", index_col=False)
         date_starts = ["2019-06-21","2019-07-15","2019-07-26","2019-08-11","2019-08-20"]
         date_ends = ["2019-06-22","2019-07-16","2019-07-27","2019-08-12","2019-08-21"]
-        # peak_diff_values = [60, 68, 62, 57, 62]
-        peak_diff_values = [61.2, 69.5, 61.3, 57.5, 62.5]
+        # peak_diff_values = [61.2, 69.5, 61.3, 57.5, 62.4]
+        peak_diff_values = [1.2, 9.5, 1.3, -2.5, 2.4]
 
         for date_start, date_end, peak_diff in tqdm(zip(date_starts, date_ends, peak_diff_values), total=5):
             temp = vel_df[vel_df['Event_Date'] == date_start]
-            DataStart = date_start
-            DataEnd = date_end
-            df_start_times = event_data[event_data[f'{station}Start'].apply(UTCDateTime).between(UTCDateTime(DataStart), UTCDateTime(DataEnd))][f'{station}Start']
-            df_start_times = df_start_times.apply(UTCDateTime)
+            DataStart, DataEnd = date_start, date_end
             data = pd.read_csv(f"{input_dir}/{date_start.replace('-','')}_Fv.csv", index_col=0)
-            data_start_time1 = UTCDateTime(data.iloc[0,-1])
-            # print(data_start_time1.datetime)
-            time_diff1 = data_start_time1 - df_start_times.iloc[0] 
             print(f"date: {date_start}, vel: {avg_velocity}, time_shift: {distance / avg_velocity : .2f}")
             data['Time'] = data.iloc[:,-1].apply(UTCDateTime) - (peak_diff * 60) + int(distance / avg_velocity)
             complete_data = pd.DataFrame(columns= ['Time'])
--- data_preprocessing/shift_utc.py
+++ data_preprocessing/shift_utc.py
@@ -10,19 +10,24 @@ import pandas as pd
 import numpy as np
 
 from obspy.core import UTCDateTime # default is UTC+0 time zone
+import sys
+sys.path.append(paths['LOCAL_BASE_DIR'])
 
 def shift_utc():
     """ Shift the time in CSV files from UTC+1 to UTC+0 by subtracting one hour.
     Reads files from RAW_LABEL_DIR and saves the modified files to UTC0_LABEL_DIR.
     """
     output_dir = f"{paths["LOCAL_BASE_DIR"]}/{paths['UTC0_LABEL_DIR']}"
-    input_dir = f"{paths["LOCAL_BASE_DIR"]}/{paths['RAW_LABEL_DIR']}/"
+    input_dir = f"{paths["LOCAL_BASE_DIR"]}/{paths['RAW_LABEL_DIR']}"
     os.makedirs(output_dir, exist_ok=True)
     for file in os.listdir(input_dir):
         data = pd.read_csv(f"{input_dir}/{file}")
         data.rename(columns={"Unnamed: 0":"Time UTC+1"}, inplace=True)
-        data["Time UTC+0"] = data['Time UTC+1'].apply(UTCDateTime) - 3600
-        data.to_csv(f"{output_dir}{file}")
+        if file in ["20190610_Fv_1.csv", "20190610_Fv_2.csv"]:
+            data["Time UTC+0"] = data['Time UTC+1'].apply(UTCDateTime) - 3600
+        else:
+            data["Time UTC+0"] = data['Time UTC+1'].apply(UTCDateTime) - 7200
+        data.to_csv(f"{output_dir}/{file}")
 
 if __name__ == "__main__":
     shift_utc()
